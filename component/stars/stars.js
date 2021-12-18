c5cb1c1f340f400844702803f3208cc41734b4d1
yinyahui <yinyahui@qding.me>
2021-12-18 23:28:04 +0800

    1.0.1-新增喜爱城市评选活动

---
+++ component/stars/stars.js
@@ -0,0 +1,66 @@
+// component/stars/starts.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    //是否可以点击
+    canSelect: {
+      type: Boolean,
+      value: true,
+    },
+    //星星个数
+    starCount: {
+      type: Number,
+      value: 5,
+    },
+    // 索引
+    index: {
+      type: Number,
+      value: 0,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    imgs: [
+      {
+        id: 1,
+      },
+      {
+        id: 2,
+      },
+      {
+        id: 3,
+      },
+      {
+        id: 4,
+      },
+      {
+        id: 5,
+      },
+    ],
+    scrNoSelect: '/images/icon/star-noSelect.png',
+    scrSelect: '/images/icon/star-select.png',
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 更新星星颜色 内部方法建议以下划线开头
+    _changeColor: function (e) {
+      let starCount = e.currentTarget.dataset.index;
+      this.setData({
+        starCount: starCount,
+      });
+      let myEventDetail = {
+        starCount: starCount,
+        index: this.data.index,
+      };
+      this.triggerEvent('myevent', myEventDetail);
+    },
+  },
+});
